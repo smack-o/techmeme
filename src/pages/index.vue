fdfb7f7fe0be47d9243dbd136e39aab36e78e205
punsuke <phunsukewonder@outlook.com>
2017-06-10 08:34:29 +0800

    初始化配置

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="hello">
-
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
+  name: 'index',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: '首页',
     };
   },
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
