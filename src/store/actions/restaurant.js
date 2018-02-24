b73fc9d9d4e9c5fe7979a4bf2708a1a30e372186
smackgg <smackgg@gmail.com>
2018-02-24 11:34:26 +0800

    update articles

--- src/store/actions/restaurant.js
+++ src/store/actions/restaurant.js
@@ -1,6 +1,8 @@
-import { GET_LIST, GET_ARTICLE } from '../types/restaurant'
+import { GET_LIST, GET_ARTICLE, GET_TOPIC_ARTICLES } from '../types/restaurant'
 import { createAction } from 'redux-actions'
 import wepy from 'wepy'
+import store from '../'
+
 export const getRestaurantList = createAction(GET_LIST, () => {
   return new Promise((resolve) => {
     wepy.request({
@@ -17,6 +19,24 @@ export const getArticle = createAction(GET_ARTICLE, (data) => {
     wepy.request({
       url: `/api/restaurant/${data.id}`,
       success (response) {
+        resolve(response.data)
+      }
+    })
+  })
+})
+
+export const getTopicArticles = createAction(GET_TOPIC_ARTICLES, (data) => {
+  console.log(store)
+  store.dispatch({
+    type: 'TOPIC_ARTICLES_LOADING'
+  })
+  return new Promise((resolve) => {
+    wepy.request({
+      url: `/api/topic/${data.id}`,
+      success (response) {
+        store.dispatch({
+          type: 'TOPIC_ARTICLES_LOADED'
+        })
         resolve(response.data)
       }
     })
--- src/store/reducers/restaurant.js
+++ src/store/reducers/restaurant.js
@@ -1,22 +1,43 @@
 import { handleActions } from 'redux-actions'
-import { GET_LIST, GET_ARTICLE } from '../types/restaurant'
+import { GET_LIST, GET_ARTICLE, GET_TOPIC_ARTICLES, TOPIC_ARTICLES_LOADING, TOPIC_ARTICLES_LOADED } from '../types/restaurant'
 
 export default handleActions({
   [GET_LIST] (state, action) {
-    console.log('reducers', state, action)
     return {
       ...state,
       list: action.payload.data
     }
   },
   [GET_ARTICLE] (state, action) {
-    console.log(action.payload)
     return {
       ...state,
       article: action.payload.data
     }
+  },
+  [GET_TOPIC_ARTICLES] (state, action) {
+    return {
+      ...state,
+      articles: action.payload.data.articles,
+      topic: action.payload.data.topic
+    }
+  },
+  [TOPIC_ARTICLES_LOADING] (state, action) {
+    console.log(111)
+    return {
+      ...state,
+      articlesLoading: true
+    }
+  },
+  [TOPIC_ARTICLES_LOADED] (state, action) {
+    return {
+      ...state,
+      articlesLoading: false
+    }
   }
 }, {
   list: [],
-  article: {}
+  article: {},
+  articles: [],
+  topic: {},
+  articlesLoading: false
 })
